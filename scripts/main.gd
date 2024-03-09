20e984c1683ab44d59d37c639bdb7f2a8c709512
Susi Fu <43230859+SusanaMG1988@users.noreply.github.com>
2024-03-09 17:27:14 +0100

    mainButtons

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,11 +1,10 @@
 extends Control
 
+func _on_options_pressed():
+	get_tree().change_scene_to_file("res://scenes/main_options.tscn")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://scenes/intro.tscn")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_credits_pressed():
+	get_tree().change_scene_to_file("res://scenes/main_credits.tscn")
